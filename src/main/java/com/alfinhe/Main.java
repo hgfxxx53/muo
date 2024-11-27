b2745cde80b43a01da907099d147003ea0fb4bec
ualfinhe <ualfinhe@gmail.com>
2024-11-27 04:46:11 +0000

    Initial commit

---
+++ src/main/java/com/alfinhe/Main.java
@@ -0,0 +1,14 @@
+package com.alfinhe;
+public class Main {
+    public static void main( String[] args ) {
+        String name = "Alfin";
+        int age = 20;
+        char gender = 'M';
+        float gpa = 3.75f;
+        boolean isMarried = false;
+
+        System.out.println ( "Name: " + name );
+        System.out.println ( "Age: " + age );
+        System.out.println ( "Gender: " + gender );
+    }
+}
